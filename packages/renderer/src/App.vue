68b37874b66b7988597ec0cd68403d27eae1193e
ginlink <ginlink1998@gmail.com>
2023-02-01 16:29:53 +0800

    feat: 增加naiveUI

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -2,53 +2,66 @@
 import ReactiveCounter from '/@/components/ReactiveCounter.vue';
 import ReactiveHash from '/@/components/ReactiveHash.vue';
 import ElectronVersions from '/@/components/ElectronVersions.vue';
+import {NButton} from 'naive-ui';
+import {darkTheme} from 'naive-ui';
+import {NConfigProvider, NGlobalStyle} from 'naive-ui';
+import {NThemeEditor} from 'naive-ui';
 
 const APP_VERSION = import.meta.env.VITE_APP_VERSION;
 </script>
 
 <template>
-  <img
-    alt="Vue logo"
-    src="../assets/logo.svg"
-    width="150"
-  />
+  <NConfigProvider :theme="darkTheme">
+    <NGlobalStyle />
+    <NThemeEditor>
+      <img
+        alt="Vue logo"
+        src="../assets/logo.svg"
+        width="150"
+      />
 
-  <p>
-    <!-- Example how to inject current app version to UI -->
-    App version: {{ APP_VERSION }}
-  </p>
+      <p>
+        <!-- Example how to inject current app version to UI -->
+        App version: {{ APP_VERSION }}
+      </p>
 
-  <p>
-    For a guide and recipes on how to configure / customize this project,<br />
-    check out the
-    <a
-      href="https://github.com/cawa-93/vite-electron-builder"
-      target="_blank"
-    >
-      vite-electron-builder documentation
-    </a>
-    .
-  </p>
+      <div>
+        <NButton type="primary">naive-ui</NButton>
+      </div>
 
-  <fieldset>
-    <legend>Test Vue Reactivity</legend>
-    <reactive-counter />
-  </fieldset>
+      <p>
+        For a guide and recipes on how to configure / customize this project,<br />
+        check out the
+        <a
+          href="https://github.com/cawa-93/vite-electron-builder"
+          target="_blank"
+        >
+          vite-electron-builder documentation
+        </a>
+        .
+      </p>
 
-  <fieldset>
-    <legend>Test Node.js API</legend>
-    <reactive-hash />
-  </fieldset>
+      <fieldset>
+        <legend>Test Vue Reactivity</legend>
+        <reactive-counter />
+      </fieldset>
 
-  <fieldset>
-    <legend>Environment</legend>
-    <electron-versions />
-  </fieldset>
+      <fieldset>
+        <legend>Test Node.js API</legend>
+        <reactive-hash />
+      </fieldset>
 
-  <p>
-    Edit
-    <code>packages/renderer/src/App.vue</code> to test hot module replacement.
-  </p>
+      <fieldset>
+        <legend>Environment</legend>
+        <electron-versions />
+      </fieldset>
+
+      <p>
+        Edit
+        <code>packages/renderer/src/App.vue</code> to test hot module replacement.
+      </p>
+    </NThemeEditor>
+  </NConfigProvider>
 </template>
 
 <style>
